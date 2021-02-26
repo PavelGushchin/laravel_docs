2964624c81326e9a93a18c5daac3e5c78c7ec33f
Pavel Gushchin <pavel_gushchin@mail.ru>
2021-02-27 03:22:06 +0700

    Fixed link

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import bs4
 import requests
 import Chapter
 
-main_page_html = requests.get('https://laravel.com/docs/8.x').text
+main_page_html = requests.get('https://laravel.com/docs/').text
 
 sidebar = bs4.BeautifulSoup(main_page_html, 'lxml', parse_only=bs4.SoupStrainer(id='indexed-nav'))
 chapters_titles = sidebar.find_all('h2')

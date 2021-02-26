0bcbbce4634524682344fbec0daa767ede0b89b9
Pavel Gushchin <pavel_gushchin@mail.ru>
2021-02-26 21:38:00 +0700

    Refactoring for Main module

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import bs4
 import requests
-import Volume
+import Chapter
 
 main_page_html = requests.get('https://laravel.com/docs/8.x').text
 
 sidebar = bs4.BeautifulSoup(main_page_html, 'lxml', parse_only=bs4.SoupStrainer(id='indexed-nav'))
-volumes_all = sidebar.find_all('h2')
-# volumes_all = sidebar.find('ul').find_all('li', recursive=False)
+chapters_titles = sidebar.find_all('h2')
+# chapters_containers = sidebar.find('ul').find_all('li', recursive=False)
 
-for volume_title_tag in volumes_all:
-    Volume.save(volume_title_tag)
+for chapter_title in chapters_titles:
+    Chapter.save(chapter_title)
